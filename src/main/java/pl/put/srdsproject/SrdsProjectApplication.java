422ae34dae9c778b380b2a70c6f0497c83a969d4
adamg <adam.grzenda@gmail.com>
2024-01-05 15:09:03 +0100

    add spring project structure

---
+++ src/main/java/pl/put/srdsproject/SrdsProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.put.srdsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrdsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrdsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/put/srdsproject/SrdsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.put.srdsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrdsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
